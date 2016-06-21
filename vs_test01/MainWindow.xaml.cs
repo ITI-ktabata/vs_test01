37bbaa2b141ddb87de4238e606a2c366479f346e
katashi-tabata <ktabata@insight-tec.co.jp>
2016-06-21 18:10:23 +0900

    hoge

--- vs_test01/MainWindow.xaml.cs
+++ vs_test01/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,16 +21,44 @@ namespace vs_test01
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            object hoge = ConfigurationManager.AppSettings["oraattusrc01"];
+            ConnectionStringSettingsCollection hoge2 = ConfigurationManager.ConnectionStrings;
+
+            // バインディング対象のインスタンスを、MainWindowViewModelに設定
+            this.DataContext = new MainWindowViewModel();
             label01.Content = OracleDao.hogehoge();
+            //listBox01.
         }
 
-        private void button01_Click(object sender, RoutedEventArgs e)
+        private async void button01_Click(object sender, RoutedEventArgs e)
         {
-            label01.Content = OracleDao.hogehoge();
-            label02.Content = textBox01.Text;
+            await Task.Run(() => label02.Content = OracleDao.hogehoge2());
+            //label02.Content = textBox01.Text;
+            
+        }
+        
+    }
+    // MainWindowクラスから完全に独立したクラス（ビューモデル）
+    public sealed class MainWindowViewModel
+    {
+        // バインディングされる値を保持するフィールド
+        private string sampleText_ = "Bound!";
+
+        // バインディング対象のプロパティ
+        public string SampleText
+        {
+            get
+            {
+                return sampleText_;
+            }
+            set
+            {
+                sampleText_ = value;
+            }
         }
     }
 }
--- vs_test01/OracleDao.cs
+++ vs_test01/OracleDao.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,7 @@ namespace vs_test01
             string str_cnt = "";
             using(var Connection = new OracleConnection()){
                 //Data SourceにApp.configの設定を参照することができます。
-                Connection.ConnectionString = "User ID=scott; Password=tiger; Data Source=oraattusrc01;";
+                Connection.ConnectionString = ConfigurationManager.ConnectionStrings["oraattusrc02"].ConnectionString;
                 Connection.Open();
 
                 //
@@ -29,6 +30,28 @@ namespace vs_test01
             }
             Console.ReadLine();
             return str_cnt;
+        }
+        public static object hogehoge2()
+        {
+            string str_cnt = "";
+            List<dynamic> hoge;
+            using (var Connection = new OracleConnection())
+            {
+                //Data SourceにApp.configの設定を参照することができます。
+                Connection.ConnectionString = ConfigurationManager.ConnectionStrings["oraattusrc01"].ConnectionString;
+                Connection.Open();
+
+                //
+                //接続した後はこれまでのODP.NETと同じ操作を行えます。
+                //Managed Driverとの機能差については以下のURLを参照。
+                //http://docs.oracle.com/cd/E57425_01/121/ODPNT/intro004.htm#CEGEJIFD
+                //
+                //ちなみにDapperも使えます。
+                hoge = Connection.Query("SELECT GRADE,LOSAL,HISAL cnt FROM salgrade").ToList();
+                str_cnt = ((Decimal)hoge[0].CNT).ToString();
+            }
+            //Console.ReadLine();
+            return hoge;
         }
     }
 }
